362d4678c151f8dc6e30bbb60504fd478a83a132
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-04 10:40:54 +0530

    implement dynamic navigation mapping

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const navigationData = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about",
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/services",
+  },
+  {
+    id: 4,
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        {navigationData.map((route) => (
+          <li className="mr-10">
+            <a href={route.path}>{route.name}</a>
+          </li>
+        ))}
+      </ul>
+
+      {/* <ul className="flex">
+        <li className="mr-10">
+          <a href="/">Home</a>
+        </li>
+        <li className="mr-10">
+          <a href="/about">About</a>
+        </li>
+        <li className="mr-10">
+          <a href="/blog">Blog</a>
+        </li>
+      </ul> */}
+    </nav>
+  );
+};
+
+export default NavBar;
