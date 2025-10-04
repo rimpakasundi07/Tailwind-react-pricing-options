6c0875c337f139b99d27c271be090b33cad25af4
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-04 23:59:39 +0530

    implement PricingOptions component with data fetching and grid layout

--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -4,19 +4,20 @@ import PricingFeatures from "./PricingFeatures";
 const PricingCard = ({ pricing }) => {
   const { name, price, description, features } = pricing;
   return (
-    <div className="border bg-blue-800 rounded-2xl p-4">
+    <div className="flex  flex-col border bg-blue-950 rounded-2xl p-4">
       {/* card header  */}
-      <div>
-        <h1 className="text-7xl">{name}</h1>
-        <h4 className="text-3xl">{price}</h4>
+      <div className="space-y-4">
+        <h1 className="text-6xl text-center">{name}</h1>
+        <h4 className="text-3xl  italic text-center">{price}</h4>
       </div>
       {/* card body */}
-      <div className="bg-sky-400 p-5 rounded-2xl mt-10">
+      <div className="bg-gradient-to-r from-[#0052D4] via-[#4364F7] to-[#6FB1FC] p-5 rounded-2xl mt-10 flex-1">
         <p>{description}</p>
         {features.map((feature, index) => (
           <PricingFeatures key={index} feature={feature}></PricingFeatures>
         ))}
       </div>
+      <button className="btn w-full mt-4">Subscribe</button>
     </div>
   );
 };
--- src/components/PricingOptions/PricingOptions.jsx
+++ src/components/PricingOptions/PricingOptions.jsx
@@ -5,9 +5,9 @@ const PricingOptions = ({ pricingPromise }) => {
   const pricingData = use(pricingPromise);
 
   return (
-    <div>
+    <div className="p-8">
       <h2 className="text-5xl">Get Our MemberShip</h2>
-      <div className="grid md:grid-cols-3 gap-8 ">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 pt-4 ">
         {pricingData.map((pricing) => (
           <PricingCard key={pricing.id} pricing={pricing}></PricingCard>
         ))}
